73c678f64f280e1aa34fa5c7d7fd7e0f7f470048
Eddie Sundvall <eddie.sundvall@gmail.com>
2018-06-01 23:01:37 +0200

    change stuffs

--- samples/dotnetapp/dotnetapp/Program.cs
+++ samples/dotnetapp/dotnetapp/Program.cs
@@ -9,7 +9,9 @@ public static class Program
 {
     public static void Main(string[] args) 
     {
-          var defaultMessage = "Hello from .NET Core!";
+        var osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+        WriteLine("Running on " + osNameAndVersion.ToString());
+          var defaultMessage = "Hello from .NET Framework!";
           var bot = GetBot();
           var (message, withColor) = ParseArgs(args);
           var output = message == string.Empty ? $"    {defaultMessage}{bot}" : $"    {message}{bot}";
